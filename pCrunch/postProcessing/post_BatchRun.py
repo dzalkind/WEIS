2ec4fe3f0be9da61fa1065e8e2dc4fd577c76e1e
dzalkind <dzalkind@nrel.gov>
2020-12-07 12:43:53 -0700

    Update post processing for own use

--- pCrunch/postProcessing/post_BatchRun.py
+++ pCrunch/postProcessing/post_BatchRun.py
@@ -4,70 +4,107 @@ A script to post process a batch run and generate stats and load rankings
 
 # Python Modules and instantiation
 import numpy as np
-import matplotlib.pyplot as plt 
-import pandas as pd
-import time
+# from wisdem.aeroelasticse.CaseLibrary import ROSCO_Test
 import os
-import pathlib
-# ROSCO toolbox modules 
-from ROSCO_toolbox import utilities as ROSCO_utilites
-fast_io = ROSCO_utilites.FAST_IO()
-fast_pl = ROSCO_utilites.FAST_Plots()
-# WISDEM modules
-from weis.aeroelasticse.Util import FileTools
-# Batch Analysis
+
+from weis.aeroelasticse.FAST_reader import InputReader_Common, InputReader_OpenFAST, InputReader_FAST7
+from weis.aeroelasticse.FAST_writer import InputWriter_Common, InputWriter_OpenFAST, InputWriter_FAST7
+from weis.aeroelasticse.runFAST_pywrapper import runFAST_pywrapper_batch
+from weis.aeroelasticse.CaseGen_General import CaseGen_General
+from weis.aeroelasticse.CaseGen_IEC import CaseGen_IEC
 from pCrunch import pdTools
 from pCrunch import Processing, Analysis
+from weis.aeroelasticse.Util import FileTools
+# Instantiate fast_IO
+from ROSCO_toolbox import utilities as ROSCO_utilities
+fast_io = ROSCO_utilities.FAST_IO()
+fast_pl = ROSCO_utilities.FAST_Plots()
+import pandas as pd
 
 
 # Define input files paths
-def post_BatchRun():
-    output_dir      = '/Users/dzalkind/Tools/WEIS/outputs/NASA/DLC_Play/'
-    results_dir     = 'results'
-    save_results    = True
-
+def post_BatchRun(test_dirs):
 
     # Load case matrix into dataframe
-    fname_case_matrix = os.path.join(output_dir,'case_matrix.yaml')
-    case_matrix = FileTools.load_yaml(fname_case_matrix, package=1)
-    cm = pd.DataFrame(case_matrix)
+    case_matrix = [None] * len(test_dir)
+    cm   = [None] * len(test_dir)
+    fastFile  = [None] * len(test_dir)
+    fastRead  = [None] * len(test_dir)
 
-    # Find all outfiles
-    outfiles = []
-    for file in os.listdir(output_dir):
-        if file.endswith('.outb'):
-            outfiles.append(output_dir + file)
-        # elif file.endswith('.out') and not file.endswith('.MD.out'):
-        #     outfiles.append(output_dir + file)
+    for i, dir in enumerate(test_dir):
+        case_matrix[i] = FileTools.load_yaml(os.path.join(test_dir[i],'case_matrix.yaml'), package=1)
+        cm[i] = pd.DataFrame(case_matrix[i])
 
-
-    # Initialize processing classes
-    fp = Processing.FAST_Processing()
-    fa = Analysis.Loads_Analysis()
-
-
-    # Set some processing parameters
-    fp.OpenFAST_outfile_list = outfiles
-    fp.t0 = 100
-    fp.parallel_analysis = True
-    fp.results_dir = os.path.join(output_dir, 'stats')
-    fp.verbose=True
-
-
-    fp.DEL_info = [('TwrBsMyt', 3), ('PtfmHeave', 3), ('PtfmPitch', 3)]
+        # Get ALL FAST info
+        fastRead[i] = InputReader_OpenFAST(FAST_ver='OpenFAST', dev_branch=True)
+        fastFile[i] = os.path.join(test_dir[i],cm[i]['Case_Name'][0] + '.fst')
+        fastRead[i].FAST_InputFile = os.path.basename(fastFile[i])   # FAST input file (ext=.fst)
+        fastRead[i].FAST_directory = os.path.dirname(fastFile[i])   # Path to fst directory files
+        fastRead[i].execute()
     
-    if save_results:
-        fp.save_LoadRanking = True
-        fp.save_SummaryStats = True
+    # Set up pCrunch, run if not already
+    reCrunch = False
 
+    name_base = 'dataset1'
+    # Do stats yaml exist?
+    stat_yaml_there = [os.path.exists(os.path.join(test,'stats',name_base + '_stats.yaml')) for test in test_dir]
+
+    outfiles = [[os.path.join(test, cn + '.outb') for cn in cm[0].Case_Name] for test in test_dir]
+
+
+
+    fp = [None]*len(test_dir)
+    st = [None]*len(test_dir)
+    lr = [None]*len(test_dir)
+    stats = [None]*len(test_dir)
+    load_rankings = [None]*len(test_dir)
     # Load and save statistics and load rankings
-    stats, load_rankings = fp.batch_processing()
+    for i_test, test in enumerate(test_dir):
+        if not stat_yaml_there[i_test] or reCrunch:
+
+            # Set some processing parameters
+            fp[i_test] = Processing.FAST_Processing()
+            fp[i_test].results_dir              = os.path.join(test, 'stats')
+            fp[i_test].OpenFAST_outfile_list    = outfiles[i_test]
+            fp[i_test].t0                       = 200         
+            fp[i_test].parallel_analysis        = False
+            fp[i_test].parallel_cores           = 8
+    #         fp[i_test].DEL_info = [('TwrBsMyt', 4)]#, ('RootMyb2', 10), ('RootMyb3', 10)]
 
 
-    print('here')
+            fp[i_test].verbose                  = False
+            fp[i_test].save_LoadRanking         = True
+            fp[i_test].save_SummaryStats        = True
+            
+            fp[i_test].ranking_vars.append(['GenPwr']), fp[i_test].ranking_stats.append('max')
+            fp[i_test].ranking_vars.append(['GenSpeed']), fp[i_test].ranking_stats.append('std')
+            fp[i_test].ranking_vars.append(['TwrBsMyt']), fp[i_test].ranking_stats.append('max')
+            fp[i_test].ranking_vars.append(['TwrBsMyt']), fp[i_test].ranking_stats.append('std')
+            fp[i_test].ranking_vars.append(['TwrBsMxt']), fp[i_test].ranking_stats.append('max')
+            fp[i_test].ranking_vars.append(['NcIMUTAxs']), fp[i_test].ranking_stats.append('std')
+            fp[i_test].ranking_vars.append(['NcIMUTAys']), fp[i_test].ranking_stats.append('std')
+
+            stats[i_test], load_rankings[i_test] = fp[i_test].batch_processing()
+            
+        # re-load to get rid of dataset
+        stats[i_test] = FileTools.load_yaml(os.path.join(os.path.join(test, 'stats'),name_base+'_stats.yaml'), package=1)
+        load_rankings[i_test] = FileTools.load_yaml(os.path.join(os.path.join(test, 'stats'),name_base+'_LoadRanking.yaml'), package=1)
+
+        # Turn into dataframe
+        st[i_test] = pdTools.dict2df(stats[i_test])
+        lr[i_test] = pdTools.dict2df(load_rankings[i_test])
+    
+
 
 if __name__ == '__main__':
-    post_BatchRun()
+
+
+    test_dir = [
+        '/Users/dzalkind/Tools/WEIS/results/NASA/sweep_wn'
+        ]
+
+
+    post_BatchRun(test_dir)
 
 
 # # Get wind speeds for processed runs
