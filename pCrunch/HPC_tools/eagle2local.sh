7415efad3b5cf81c8d911dbfab13a1bd7d5667e9
dzalkind <dzalkind@nrel.gov>
2020-12-09 11:46:42 -0700

    Use eagle2local script

--- pCrunch/HPC_tools/eagle2local.sh
+++ pCrunch/HPC_tools/eagle2local.sh
@@ -1,15 +1,15 @@
 # ------ Sync DLC Data from eagle runs locally -------
 
 # --- 5MW LAND LEGACY ---
-outdir='/projects/ssc/nabbas/DLC_Analysis/5MW_Land/'
-indir='../BatchOutputs/5MW_Land/5MW_Land_legacy/'
+outdir='/scratch/dzalkind/WEIS-3/results/CT-spar/DISCON_CT-spar_lowBW/steps/'
+indir='/Users/dzalkind/Tools/WEIS-3/results/CT-spar/DISCON_CT-spar_lowBW/steps/'
 mkdir -p $indir;
-rsync nabbas@eagle.hpc.nrel.gov:$outdir*.outb $indir
-rsync nabbas@eagle.hpc.nrel.gov:$outdir/case_matrix.yaml $indir
+rsync -aP --no-g dzalkind@eagle.hpc.nrel.gov:$outdir $indir
+# rsync dzalkind@eagle.hpc.nrel.gov:$outdir/case_matrix.yaml $indir
 
-# --- 5MW LAND ROSCO ---
-outdir2='/projects/ssc/nabbas/DLC_Analysis/5MW_OC3Spar'
-indir2='../BatchOutputs/5MW_Land/5MW_Land_ROSCO/'
-mkdir -p $indir2;
-rsync nabbas@eagle.hpc.nrel.gov:$outdir2*.outb $indir2
-rsync nabbas@eagle.hpc.nrel.gov:$outdir2/case_matrix.yaml $indir2
+# # --- 5MW LAND ROSCO ---
+# outdir2='/projects/ssc/nabbas/DLC_Analysis/5MW_OC3Spar'
+# indir2='../BatchOutputs/5MW_Land/5MW_Land_ROSCO/'
+# mkdir -p $indir2;
+# rsync nabbas@eagle.hpc.nrel.gov:$outdir2*.outb $indir2
+# rsync nabbas@eagle.hpc.nrel.gov:$outdir2/case_matrix.yaml $indir2
