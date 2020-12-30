694d9195051f8862e6bebdc1a1a54fdb513d0001
dzalkind <dzalkind@nrel.gov>
2020-12-30 12:03:59 -0700

    Update post processing scripts - will break pCrunch subtree!

--- pCrunch/HPC_tools/eagle2local.sh
+++ pCrunch/HPC_tools/eagle2local.sh
@@ -1,11 +1,42 @@
 # ------ Sync DLC Data from eagle runs locally -------
 
 # --- 5MW LAND LEGACY ---
-outdir='/scratch/dzalkind/WEIS-3/results/CT-spar/DISCON_CT-spar_lowBW/steps/'
-indir='/Users/dzalkind/Tools/WEIS-3/results/CT-spar/DISCON_CT-spar_lowBW/steps/'
+# outdir='/scratch/dzalkind/WEIS-3/results/'
+# indir='/Users/dzalkind/Tools/WEIS-3/results/'
+# mkdir -p $indir;
+# rsync -aP --no-g --include="*/" --include="*.yaml" --exclude="*" dzalkind@eagle.hpc.nrel.gov:$outdir $indir
+# # rsync dzalkind@eagle.hpc.nrel.gov:$outdir/case_matrix.yaml $indir
+
+outdir='/scratch/dzalkind/WEIS-3/results/'
+indir='/Users/dzalkind/Tools/WEIS-3/results/'
 mkdir -p $indir;
-rsync -aP --no-g dzalkind@eagle.hpc.nrel.gov:$outdir $indir
-# rsync dzalkind@eagle.hpc.nrel.gov:$outdir/case_matrix.yaml $indir
+# rsync -aP --no-g dzalkind@eagle.hpc.nrel.gov:$outdir $indir
+rsync -aP --no-g --include="*/" --include="*.yaml" --exclude="*" dzalkind@eagle.hpc.nrel.gov:$outdir $indir
+
+
+# outdir='/scratch/dzalkind/WEIS-3/results/CT-spar/DISCON-CT-spar_90/simp+pc_mode/'
+# indir='/Users/dzalkind/Tools/WEIS-3/results/CT-spar/DISCON-CT-spar_90/simp+pc_mode/'
+# mkdir -p $indir;
+# rsync -aP --no-g dzalkind@eagle.hpc.nrel.gov:$outdir $indir
+
+# outdir='/scratch/dzalkind/WEIS-3/results/CT-semi/DISCON-CT-semi_90/simp+pc_mode/'
+# indir='/Users/dzalkind/Tools/WEIS-3/results/CT-semi/DISCON-CT-semi_90/simp+pc_mode/'
+# mkdir -p $indir;
+# rsync -aP --no-g dzalkind@eagle.hpc.nrel.gov:$outdir $indir
+
+# outdir='/scratch/dzalkind/WEIS-3/results/CT-barge/DISCON-CT-barge_90/simp+pc_mode/'
+# indir='/Users/dzalkind/Tools/WEIS-3/results/CT-barge/DISCON-CT-barge_90/simp+pc_mode/'
+# mkdir -p $indir;
+# rsync -aP --no-g dzalkind@eagle.hpc.nrel.gov:$outdir $indir
+
+# outdir='/scratch/dzalkind/WEIS-3/results/CT-TLP/DISCON-CT-TLP_90/simp+pc_mode/'
+# indir='/Users/dzalkind/Tools/WEIS-3/results/CT-TLP/DISCON-CT-TLP_90/simp+pc_mode/'
+# mkdir -p $indir;
+# rsync -aP --no-g dzalkind@eagle.hpc.nrel.gov:$outdir $indir
+
+# rsync -aP --no-g --include="*/" --include="*.yaml" --exclude="*" dzalkind@eagle.hpc.nrel.gov:$outdir $indir
+
+# /scratch/dzalkind/WEIS-3/results/CT-barge/DISCON-CT-barge/ntm
 
 # # --- 5MW LAND ROSCO ---
 # outdir2='/projects/ssc/nabbas/DLC_Analysis/5MW_OC3Spar'
