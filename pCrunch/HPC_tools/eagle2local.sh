816a954dc958c37ea7f6d6c68ad0386efb003d67
dzalkind <dzalkind@nrel.gov>
2020-12-30 11:55:45 -0700

    Only grab summary yaml files from eagle

--- pCrunch/HPC_tools/eagle2local.sh
+++ pCrunch/HPC_tools/eagle2local.sh
@@ -1,10 +1,15 @@
 # ------ Sync DLC Data from eagle runs locally -------
 
 # --- 5MW LAND LEGACY ---
-outdir='/scratch/dzalkind/WEIS-1/results/NASA/c_pitch/'
-indir='/Users/dzalkind/Tools/WEIS/results/NASA/c_pitch/'
+# outdir='/scratch/dzalkind/WEIS-1/results/NASA/c_pitch/'
+# indir='/Users/dzalkind/Tools/WEIS/results/NASA/c_pitch/'
+# mkdir -p $indir;
+# rsync -aP --no-g dzalkind@eagle.hpc.nrel.gov:$outdir $indir
+
+outdir='/scratch/dzalkind/WEIS-1/results/NASA/'
+indir='/Users/dzalkind/Tools/WEIS-1/results/NASA/'
 mkdir -p $indir;
-rsync -aP --no-g dzalkind@eagle.hpc.nrel.gov:$outdir $indir
+rsync -aP --no-g --include="*/" --include="*.yaml" --exclude="*" dzalkind@eagle.hpc.nrel.gov:$outdir $indir
 
 # # --- 5MW LAND ROSCO ---
 # outdir2='/projects/ssc/nabbas/DLC_Analysis/5MW_OC3Spar'
